6f00738aa0ba66f5f95d0f42881fcd7550efba38
ufasik <dimabuharov12@gmail.com>
2023-08-01 15:37:06 +0400

    target

--- src/main/java/com/example/newMock3/Controller/MainController.java
+++ src/main/java/com/example/newMock3/Controller/MainController.java
@@ -14,6 +14,8 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.math.BigDecimal;
+import java.math.RoundingMode;
+import java.util.Random;
 
 @RestController
 public class MainController {
@@ -32,22 +34,31 @@ public class MainController {
             String clientId = requestDTO.getClientId();
             char firstDigit = clientId.charAt(0);
             BigDecimal maxLimit;
+            String currency;
+
+
 
             if (firstDigit == '8') {
                 maxLimit = new BigDecimal(2000.00);
+                currency = "US";
             } else if (firstDigit == '9') {
                 maxLimit = new BigDecimal(1000.00);
+                currency = "EU";
             } else {
                 maxLimit = new BigDecimal(10000.00);
+                currency = "RUB";
             }
 
             String RqUID = requestDTO.getRqUID();
+            String account = requestDTO.getAccount();
 
             ResponseDTO responseDTO = new ResponseDTO();
             responseDTO.setRqUID(RqUID);
             responseDTO.setClientId(clientId);
-            responseDTO.setAccount(responseDTO.getAccount());
-            responseDTO.setCurrency("RUB");
+            responseDTO.setAccount(requestDTO.getAccount());
+            responseDTO.setCurrency(currency);
+            BigDecimal balance = getRandomBalance(maxLimit);
+            responseDTO.setBalance(balance.setScale(2, RoundingMode.HALF_UP).toString());
             responseDTO.setMaxLimit(maxLimit);
 
             log.error("***** Запрос *****" + mapper.writerWithDefaultPrettyPrinter().writeValueAsString(requestDTO));
@@ -57,5 +68,11 @@ public class MainController {
         } catch (Exception e) {
             return ResponseEntity.status(HttpStatus.BAD_REQUEST).body((e.getMessage()));
         }
+    }
+
+    private BigDecimal getRandomBalance(BigDecimal maxLimit) {
+        Random random = new Random();
+        double balance = random.nextDouble() * maxLimit.doubleValue();
+        return new BigDecimal(balance).setScale(2, RoundingMode.HALF_UP);
     }
 }
